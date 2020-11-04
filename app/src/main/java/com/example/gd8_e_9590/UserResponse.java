55fab854b19613f80a66288efa6cdec6cdaf4439
WilliBuli17 <buliwilli17@gmail.com>
2020-11-04 07:41:10 +0700

    master file

---
+++ app/src/main/java/com/example/gd8_e_9590/UserResponse.java
@@ -0,0 +1,32 @@
+package com.example.gd8_e_9590;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class UserResponse {
+    @SerializedName("data")
+    @Expose
+    private List<UserDAO> users = null;
+
+    @SerializedName("message")
+    @Expose
+    private String message;
+
+    public List<UserDAO> getUsers() {
+        return users;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setUsers(List<UserDAO> users) {
+        this.users = users;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
